1622b39c355b24ecdf58edb43c6415b5fc21f069
Sebastián Rodrigo Najle <103438231+Sebas202070@users.noreply.github.com>
2025-02-28 10:01:09 -0300

    Fondo

--- app/page.js
+++ app/page.js
@@ -3,12 +3,15 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="relative h-screen">
-      <Image src="/images/pizza-background.jpg" alt='' layout="fill" objectFit="cover" />
+      <Image src="/images/pizza1.jpg" alt='' layout="fill" objectFit="cover" />
       <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="absolute inset-0 flex flex-col justify-center items-center">
-      {/*   <Image src="/images/pizza-jai-logo2.jpg" alt="Pizza Jai Logo" width={200} height={200} /> */}
-        <h1 className="text-white border-red-500 text-6xl font-bold -mt-32">¡Bienvenido a Pizza Jai Delivery!</h1>
-        <h2 className="text-white text-3xl font-bold mt-4">¡Hace tu pedido ya!</h2>
+      <div className="absolute inset-0 flex flex-col justify-center items-center -mt-54">
+      
+        <h1 className="text-white text-6xl font-bold mt-8 text-center">¡Bienvenido a Pizza Jai Delivery!</h1> {/* Agregado text-center */}
+        <h2 className="text-white text-3xl font-bold mt-4 text-center">¡Hace tu pedido ya!</h2> {/* Agregado text-center */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-8">
+          {/* ... (cards de pizza) ... */}
+        </div>
       </div>
     </div>
   );
