c8b2fae272a8be875cd51671f54d7d225a15c010
Sebastián Rodrigo Najle <103438231+Sebas202070@users.noreply.github.com>
2025-02-28 10:07:27 -0300

    Fondo1

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="relative h-screen">
       <Image src="/images/pizza1.jpg" alt='' layout="fill" objectFit="cover" />
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="absolute inset-0 bg-black opacity-40"></div>
       <div className="absolute inset-0 flex flex-col justify-center items-center -mt-54">
       
         <h1 className="text-white text-6xl font-bold mt-8 text-center">¡Bienvenido a Pizza Jai Delivery!</h1> {/* Agregado text-center */}
